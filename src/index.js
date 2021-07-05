863b120f3f3097fe46941f10fe7554e5f43740d2
Oleh Yenko <olenko501@gmail.com>
2021-07-05 23:23:28 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,4 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
