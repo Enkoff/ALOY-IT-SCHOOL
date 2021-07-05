b8d87157ce1047f5f5f0b5b06dc9c256879a78a1
Oleh Yenko <olenko501@gmail.com>
2021-07-05 22:43:28 +0300

    delete file

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
-import { userReducer } from "./userReducer";
-import thunk from "redux-thunk";
-
-const rootReducer = combineReducers({
-  userReducer
-});
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
