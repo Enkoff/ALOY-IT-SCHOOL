daea98a538f93bea88ccd6501d9309b963300997
Oleh Yenko <olenko501@gmail.com>
2021-07-08 15:58:37 +0300

    add link in start drawer

--- src/components/Header/Drawer/StartDrawer.js
+++ src/components/Header/Drawer/StartDrawer.js
@@ -4,43 +4,62 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  makeStyles
 } from "@material-ui/core/";
 import AssignmentIcon from "@material-ui/icons/Assignment";
-import InfoIcon from '@material-ui/icons/Info';
-import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
-import LayersIcon from '@material-ui/icons/Layers';
+import InfoIcon from "@material-ui/icons/Info";
+import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
+import LayersIcon from "@material-ui/icons/Layers";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    textDecoration: "none",
+    color: theme.palette.text.primary,
+  },
+}));
 
 const StartDrawer = (props) => {
+  const classes = useStyles();
+
   return (
     <>
       <ListItem alignItems="center">
         <ListItemText secondary={"ТУТ БУДЕ ЛОГОТИП"} />
       </ListItem>
       <Divider />
-      <ListItem>
-        <ListItemIcon>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary={"Новини"} />
-      </ListItem>
-      <ListItem>
-        <ListItemIcon>
-          <LayersIcon />
-        </ListItemIcon>
-        <ListItemText primary={"Курси"} />
-      </ListItem>
-      <ListItem>
-        <ListItemIcon>
-          <EmojiPeopleIcon />
-        </ListItemIcon>
-        <ListItemText primary={"Контакти"} />
-      </ListItem>
-      <ListItem>
-        <ListItemIcon>
-          <InfoIcon />
-        </ListItemIcon>
-        <ListItemText primary={"Про нас"} />
-      </ListItem>
+      <Link to="/news" className={classes.link}>
+        <ListItem button>
+          <ListItemIcon>
+            <AssignmentIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Новини"} />
+        </ListItem>
+      </Link>
+      <Link to="/" className={classes.link}>
+        <ListItem button>
+          <ListItemIcon>
+            <LayersIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Курси"} />
+        </ListItem>
+      </Link>
+      <Link to="/" className={classes.link}>
+        <ListItem button>
+          <ListItemIcon>
+            <EmojiPeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Контакти"} />
+        </ListItem>
+      </Link>
+      <Link to="/" className={classes.link}>
+        <ListItem button>
+          <ListItemIcon>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Про нас"} />
+        </ListItem>
+      </Link>
     </>
   );
 };
