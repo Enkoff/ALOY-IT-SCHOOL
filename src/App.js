5de65310eb1bb5bcad9792756fefcbf0508b049d
Oleh Yenko <olenko501@gmail.com>
2021-07-17 01:20:03 +0300

    add admin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import News from "./Pages/News/News-page";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
@@ -10,28 +10,47 @@ import ProfilePage from "./Pages/User/Profile/Profile-page";
 import Dashboard from "./Pages/User/Dashboard/Dashboard";
 import Schedule from "./Pages/User/Schedule/Schedule";
 import { auth } from "./redux/authActions";
+import AdminDrawer from "./admin/AdminDrawer";
+import AddPage from "./admin/add-page/AddPage";
+import UsersPage from "./admin/users-page/UsersPage";
+import AdminStartPage from './admin/AdminStartPage';
 
 function App() {
   const dispatch = useDispatch();
+  const role = useSelector((store) => store.userReducer.role);
+  // const isAuth = useSelector(store => store.auth.isAuth)
 
   useEffect(() => {
     dispatch(auth());
   }, [dispatch]);
 
+  if (role === "admin") {
+    return (
+      <Router>
+        <AdminDrawer />
+        <Switch>
+          <Route exact path="/" component={AdminStartPage} />
+          <Route exact path="/admin-add-page" component={AddPage} />
+          <Route exact path="/admin-users-page" component={UsersPage} />
+        </Switch>
+      </Router>
+    );
+  }
+
   return (
     <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={StartScreen} />
-          <Route exact path="/news" component={News} />
-          <Route exact path="/profile" component={ProfilePage} />
-          <Route exact path="/rang" component={Dashboard} />
-          <Route exact path="/schedule" component={Schedule} />
-          <Route exact path="/education-material" component={StartScreen} />
-          <Route exact path="/home-work" component={StartScreen} />
-          <Route exact path="/pay" component={StartScreen} />
-        </Switch>
-        <Footer />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={StartScreen} />
+        <Route exact path="/news" component={News} />
+        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/rang" component={Dashboard} />
+        <Route exact path="/schedule" component={Schedule} />
+        <Route exact path="/education-material" component={StartScreen} />
+        <Route exact path="/home-work" component={StartScreen} />
+        <Route exact path="/pay" component={StartScreen} />
+      </Switch>
+      <Footer />
     </Router>
   );
 }
